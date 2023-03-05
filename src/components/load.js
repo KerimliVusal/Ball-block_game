0f91861e87647501cdb334c7909ead54b3820a07
Kerimli Vusal <108870798+KerimliVusal@users.noreply.github.com>
2023-03-05 21:09:32 +0400

    last

--- src/components/load.js
+++ src/components/load.js
@@ -1,8 +1,9 @@
 import React from "react";
+const gifs=require('../../public/breakout-tutorial.gif')
 const Load=()=>{
     return(<div className='loa'>
             <h1 className='animate'>
-            <div><img src={"/breakout-tutorial.gif"}  alt="gif loadergi" className='img'/></div>
+            <div><img src={gifs}  alt="gif loadergi" className='img'/></div>
                <div>Next Movies . . .</div></h1>
     </div>
     );
