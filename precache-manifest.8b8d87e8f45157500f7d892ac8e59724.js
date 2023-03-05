7b223fccab52a15c4f86ccede0b55cd76c31362e
KerimliVusal <kerimlivusal98@gmail.com>
2023-03-05 20:12:04 +0400

    Updates

---
+++ precache-manifest.8b8d87e8f45157500f7d892ac8e59724.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "10bc73ef74239fb81240b61339bd1f50",
+    "url": "/Ball-block_game/index.html"
+  },
+  {
+    "revision": "b6dc3fcabea7e9ab6df0",
+    "url": "/Ball-block_game/static/css/main.e9441108.chunk.css"
+  },
+  {
+    "revision": "a4babb6a2963ca1c38f5",
+    "url": "/Ball-block_game/static/js/2.c25a89c9.chunk.js"
+  },
+  {
+    "revision": "b6dc3fcabea7e9ab6df0",
+    "url": "/Ball-block_game/static/js/main.7d76e83d.chunk.js"
+  },
+  {
+    "revision": "c3c89da5f879c9319910",
+    "url": "/Ball-block_game/static/js/runtime~main.c8e0f6ce.js"
+  }
+]);
